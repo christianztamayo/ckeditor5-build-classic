3e08c9bb50de13131cb5e338861ef3aa1e36c7e3
Christian Z. Tamayo <christianztamayo@gmail.com>
2019-12-08 01:34:35 +0800

    Feature: Add Placeholder plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -23,6 +23,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Placeholder from './plugins/placeholder/placeholder';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -43,6 +44,7 @@ ClassicEditor.builtinPlugins = [
 	List,
 	Paragraph,
 	PasteFromOffice,
+	Placeholder,
 	Table,
 	TableToolbar
 ];
